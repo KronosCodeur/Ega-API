73c9c1dd285a90317fa714b619cbfc9c3b25f14a
KronosCodeur <piraliarris@gmail.com>
2024-02-01 14:22:14 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# EGA API
+### Thematic
+Api for an online bank app call Ega
+### Stack
+* API: Spring boot
+* Documentation : Swagger && Jetbrains WriteSide
+* Front: Angular 17
+* Database: MySQL
+* Docker
---
+++ src/test/java/tech/dracarys/egaapi/EgaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dracarys.egaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
